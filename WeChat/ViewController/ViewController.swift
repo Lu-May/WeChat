eb10f722fb11832d1f5b488953ed223aaecc6d8d
Yuehuan Lu <yuehuan.lu@YuehuandeMacBook-Pro.local>
2021-01-28 21:40:29 +0800

    [luyuehuan] feat:pull to load more datas

--- WeChat/ViewController/ViewController.swift
+++ WeChat/ViewController/ViewController.swift
@@ -10,12 +10,26 @@ import UIKit
 class ViewController: UIViewController {
   @IBOutlet weak var tableView: UITableView!
 
+  private let refreshControl = UIRefreshControl()
   let viewModel = ViewModel()
+  
+  @objc private func refreshTableViewData(_ sender: Any) {
+    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(2)) {
+      self.viewModel.initDatas()
+      self.viewModel.getTableViewDataSource()
+      self.tableView.reloadData()
+      self.refreshControl.endRefreshing()
+    }
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
     self.title = "朋友圈"
         
+    tableView.refreshControl = refreshControl
+    
+    refreshControl.addTarget(self, action: #selector(refreshTableViewData(_:)), for: .valueChanged)
+    
     viewModel.getTweetDatas() { [weak self] in
       self?.tableView.reloadData()
     }
@@ -53,15 +67,28 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return viewModel.tweetDatas?.count ?? 0
+    return viewModel.tableViewDatas.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     guard let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as? TableViewCell else {
       return UITableViewCell()
     }
-    cell.configure(with: viewModel.tweetDatas?[indexPath.row] ?? Tweet())
+    cell.configure(with: viewModel.tableViewDatas[indexPath.row] )
     return cell
+  }
+  
+  func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+    let currentOffset = tableView.contentOffset.y
+    let maximumOffset = tableView.contentSize.height - tableView.frame.size.height
+    
+    if maximumOffset - currentOffset <= 10.0 {
+      DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(1)) {
+        print("reload")
+        self.viewModel.getTableViewDataSource()
+        self.tableView.reloadData()
+      }
+    }
   }
 }
 
--- WeChat/ViewModel/ViewModel.swift
+++ WeChat/ViewModel/ViewModel.swift
@@ -9,6 +9,7 @@ import Foundation
 
 class ViewModel {
   var tweetDatas: [Tweet]?
+  var tableViewDatas = [Tweet]()
   var profile: Profile?
 
   private let tweetNetworkClient: TweetNetworkClient = .init()
@@ -18,6 +19,7 @@ class ViewModel {
     tweetNetworkClient.request(url: URL(string: "https://emagrorrim.github.io/mock-api/moments.json")!){ json, _ in
       self.tweetDatas = json as? [Tweet]
       self.tweetDatas = self.tweetDatas?.filter( { $0.images != nil || $0.content != nil } )
+      self.getTableViewDataSource()
       completion()
     }
   }
@@ -26,6 +28,28 @@ class ViewModel {
     profileNetworkClient.request(url: URL(string: "https://emagrorrim.github.io/mock-api/user/jsmith.json")!) { json, _ in
       self.profile = json as? Profile
       completion()
+    }
+  }
+  
+  func initDatas() {
+    self.tweetDatas = tableViewDatas
+    tableViewDatas = []
+  }
+  
+  func getTableViewDataSource() {
+    if tweetDatas?.count ?? 0 / 5 > 0 {
+      for var i in 0..<5 {
+        i += 1
+        tableViewDatas.append(tweetDatas?.first ?? Tweet())
+        tweetDatas?.removeFirst()
+      }
+    } else if tweetDatas?.count ?? 0 / 5 == 0 {
+      if let datas = tweetDatas {
+        for data in datas {
+          tableViewDatas.append(data)
+        }
+        tweetDatas = []
+      }
     }
   }
 }
